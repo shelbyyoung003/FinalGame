d307af3775b0ca156074ab4ee208ee0aef261067
Shelby <shelbyyoung003@gmail.com>
2018-10-13 21:06:46 -0600

    Added Mouse Event and Float Data

---
+++ Final Game/Assets/Scripts/MouseEventBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEventBehaviour : MonoBehaviour
+{
+    public UnityEvent MouseDown;
+    public UnityEvent MouseUp;
+
+    private void OnMouseDown ()
+    {
+        MouseDown.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        MouseUp.Invoke();
+    }
+}
